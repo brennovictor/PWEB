2cec031df36b80fe2e1f900bdf48b9faef5e300a
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-06-26 17:39:08 -0300

    Add files via upload

---
+++ next2/pages/index.js
@@ -0,0 +1,17 @@
+export function function_1(){
+
+    return (
+ 
+       <div>
+ 
+          <h2>
+ 
+            Viva Serra Negra!
+ 
+          </h2>
+ 
+       </div>
+ 
+    )
+ 
+ }
---
+++ next2/pages/index3.js
@@ -0,0 +1,27 @@
+import function {function_1} from './index.js';
+import function {function_2} from './index2.js';
+
+
+export default function Home() {
+    return (
+ 
+       <div>
+            {function_1()}
+            {function_2()}
+          <h2>
+ 
+            Viva todo mundo!
+ 
+          </h2>
+ 
+       </div>
+ 
+    )
+ 
+ }
+ 
+ 
+ 
+ 
+
+ 
